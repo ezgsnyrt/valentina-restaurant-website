1d2ebe8195e94609a2876ded24e83dbdb745a612
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-06-26 01:37:12 +0200

    make accordion galery interactive

--- src/components/SpecialitiesComponent.jsx
+++ src/components/SpecialitiesComponent.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+// import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-// import React, { useEffect, useRef } from 'react';
-
-let cards = document.querySelectorAll('.image-card')
-
-cards.forEach(card => card.addEventListener('click', () => {
-  cards.forEach(card => card.classList.remove('active'));
-  card.classList.add('active');
-}));
+import React, { useRef } from 'react';
 
 
-function SpecialitiesComponent () {
+
+function SpecialitiesComponent ({articles}) {
+
+  const handleClick = (event)  => {
+    console.log(event)
+    if (event.currentTarget.className !== 'image-card active') {
+      event.currentTarget.classList = 'image-card active';
+    } else {
+      event.currentTarget.className = 'image-card'
+    }
+  };
+
+
   return (
     <section id="section-specialities">
       <div className="inner-section-container">
@@ -25,42 +30,25 @@ function SpecialitiesComponent () {
           </Row>
           <Row className="bottom-row">
             <Col className="bottom-col">
-            <article className="image-card active">
-              <Image className="individual-image" src=".././assets/service-image-three.jpg"  alt="chef" />
-              <div className="image-card-details">
-                <div className="card-profile">
-                  <Image src=".././assets/service-image-two.jpg"  alt="user" fluid />
-                </div>
-                <div className="card-profile-info">
-                  <h2>Food Name</h2>
-                  <p>Image description here</p>
-                </div>
-              </div>
-            </article>
-            <article className="image-card active">
-              <Image className="individual-image" src=".././assets/service-image-one.jpg"  alt="chef" />
-              <div className="image-card-details">
-                <div className="card-profile">
-                  <Image src=".././assets/service-image-two.jpg"  alt="user" fluid />
-                </div>
-                <div className="card-profile-info">
-                  <h2>Food Name</h2>
-                  <p>Image description here</p>
-                </div>
-              </div>
-            </article>
-            <article className="image-card active">
-              <Image className="individual-image" src=".././assets/service-image-one.jpg"  alt="chef" />
-              <div className="image-card-details">
-                <div className="card-profile">
-                  <Image src=".././assets/service-image-two.jpg"  alt="user" fluid />
-                </div>
-                <div className="card-profile-info">
-                  <h2>Food Name</h2>
-                  <p>Image description here</p>
-                </div>
-              </div>
-            </article>
+              {
+                articles.map((article) => (
+                  <article className="image-card" onClick={handleClick}>
+                    <Image
+                      className="individual-image"
+                      src=".././assets/service-image-three.jpg"
+                      alt="chef" />
+                    <div className="image-card-details">
+                      <div className="card-profile">
+                        <Image src=".././assets/service-image-two.jpg"  alt="user" fluid />
+                      </div>
+                      <div className="card-profile-info">
+                        <h2>Food Name</h2>
+                        <p>Image description here</p>
+                      </div>
+                    </div>
+                  </article>
+                ))
+              }
             </Col>
           </Row>
       </Container>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,15 +4,18 @@ import AboutComponent from "../components/AboutComponent";
 import ServiceComponent from '../components/ServiceComponent';
 import ChefComponent from '../components/ChefComponent';
 import SpecialitiesComponent from '../components/SpecialitiesComponent';
+import { useState } from 'react';
 
 
 function HomePage(){
+  const [articles,setArticles] = useState([1,2,3])
+
   return (
     <div>
       <HeaderComponent />
       <AboutComponent />
       <ChefComponent />
-      <SpecialitiesComponent />
+      <SpecialitiesComponent articles={articles} />
       <ServiceComponent />
       <div>Home Page</div>
     </div>
