f88f63fb28d1633afe56afec7e0b1d36314c6f17
Ezgi Senyurt-Ertürk <156966522+ezgsnyrt@users.noreply.github.com>
2024-06-26 15:29:54 +0200

    revise specialities component's content and styles, add bakcground image to the component

--- src/components/SpecialitiesComponent.jsx
+++ src/components/SpecialitiesComponent.jsx
@@ -29,9 +29,6 @@ function SpecialitiesComponent ({articles}) {
         <h2 className="section-title">Our Specialities</h2>
       </div>
       <Container>
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
         <Row className="bottom-row">
           <Col className="bottom-col">
             {articles.map((article, index) => (
@@ -48,7 +45,7 @@ function SpecialitiesComponent ({articles}) {
                 />
                 <div className="image-card-details">
                   <div className="card-profile">
-                    <Image src=".././assets/service-image-two.jpg" alt="user" fluid />
+                    <Image src=".././assets/card-profile.jpg" alt="user" fluid />
                   </div>
                   <div className="card-profile-info">
                     <h2>Food Name</h2>
@@ -65,58 +62,6 @@ function SpecialitiesComponent ({articles}) {
 }
 
 
-
-
-
-
-// function SpecialitiesComponent ({articles}) {
-
-//   const handleClick = (event)  => {
-//     console.log(event)
-//     if (event.currentTarget.className !== 'image-card active') {
-//       event.currentTarget.className = 'image-card active';
-//     } else {
-//       event.currentTarget.className = 'image-card'
-//     }
-//   };
-
-
-
-//   return (
-//     <section id="section-specialities">
-//       <div className="inner-section-container">
-//         <h2 className="section-title">Our Specialities</h2>
-//       </div>
-//       <Container>
-//           <Row>
-//             <Col>1 of 1</Col>
-//           </Row>
-//           <Row className="bottom-row">
-//             <Col className="bottom-col">
-//               {
-//                 articles.map((article) => (
-//                   <article className="image-card" onClick={handleClick}>
-//                     <Image
-//                       className="individual-image"
-//                       src=".././assets/service-image-three.jpg"
-//                       alt="chef" />
-//                     <div className="image-card-details">
-//                       <div className="card-profile">
-//                         <Image src=".././assets/service-image-two.jpg"  alt="user" fluid />
-//                       </div>
-//                       <div className="card-profile-info">
-//                         <h2>Food Name</h2>
-//                         <p>Image description here</p>
-//                       </div>
-//                     </div>
-//                   </article>
-//                 ))
-//               }
-//             </Col>
-//           </Row>
-//       </Container>
-//     </section>
-//   )
-// }
-
 export default SpecialitiesComponent;
+
+//<a href="https://www.vecteezy.com/free-vector/signature">Signature Vectors by Vecteezy</a>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,13 +10,31 @@ import { useState } from 'react';
 function HomePage(){
   const [articles,setArticles] = useState([
     {
-      "img_src": ".././assets/service-image-one.jpg"
+      "img_src": ".././assets/specialities-image-one.jpg"
     },
     {
-      "img_src": ".././assets/service-image-two.jpg"
+      "img_src": ".././assets/specialities-image-two.jpg"
     },
     {
-      "img_src": ".././assets/service-image-three.jpg"
+      "img_src": ".././assets/specialities-image-three.jpg"
+    },
+    {
+      "img_src": ".././assets/specialities-image-four.jpg"
+    },
+    {
+      "img_src": ".././assets/specialities-image-five.jpg"
+    },
+    {
+      "img_src": ".././assets/specialities-image-six.jpg"
+    },
+    {
+      "img_src": ".././assets/specialities-image-seven.jpg"
+    },
+    {
+      "img_src": ".././assets/specialities-image-eight.jpg"
+    },
+    {
+      "img_src": ".././assets/specialities-image-nine.jpg"
     }
   ])
 
@@ -34,3 +52,14 @@ function HomePage(){
 }
 
 export default HomePage
+
+
+//<a href="https://www.vecteezy.com/free-photos/elegant-food">Elegant Food Stock photos by Vecteezy</a>
+//<a href="https://www.vecteezy.com/free-photos/elegant-food">Elegant Food Stock photos by Vecteezy</a>
+//<a href="https://www.vecteezy.com/free-photos/chef">Chef Stock photos by Vecteezy</a>
+//Photo by <a href="https://unsplash.com/@nahimaaparicio?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nahima Aparicio</a> on <a href="https://unsplash.com/photos/a-white-plate-topped-with-french-toast-covered-in-fruit-HO7RFYSsFfw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//<a href="https://www.vecteezy.com/free-photos/nature-photography">Nature Photography Stock photos by Vecteezy</a>
+//<a href="https://www.vecteezy.com/free-photos/veg">Veg Stock photos by Vecteezy</a>
+//<a href="https://www.vecteezy.com/free-photos/dish">Dish Stock photos by Vecteezy</a>
+//<a href="https://www.vecteezy.com/free-photos/ai-generated">Ai Generated Stock photos by Vecteezy</a>
+//<a href="https://www.vecteezy.com/free-photos/ai">Ai Stock photos by Vecteezy</a>
