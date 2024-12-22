e7991f080c4b7e791cc5a227ec97b90391f88365
ezgsnyrt <ezgi.senyurt3@gmail.com>
2024-12-22 18:30:29 +0100

    updated NavbarComponent.jsx to make it responsive by using boostrap features and media queries

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faV } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 function NavbarComponent() {
   return (
-    <nav>
-      <div className="navigation-logo">
-        <FontAwesomeIcon icon={faV} size={"lg"} color={"white"} />
-        <a href="brand">ALENTINA</a>
-      </div>
-      <div className="navigation-menu">
-        <ul className="navigation-links">
-          <li><Link to="/./pages/">Home</Link></li>
-          <li><Link to="/./pages/about">Our Story</Link></li>
-          <li><Link to="/./pages/menu">Menu</Link></li>
-          <li><Link to="/./pages/services">Services</Link></li>
-          <li><Link to="/./pages/reservation">Reservation</Link></li>
-          <li><Link to="/./pages/blog">Blog</Link></li>
-          <li><Link to="/./pages/contact">Contact</Link></li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home" className="navigation-logo">
+          <FontAwesomeIcon icon={faV} size={"lg"} color={"white"} className="me-2" />
+          <span>ALENTINA</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto navigation-menu">
+            <Nav.Link as={Link} to="/./pages/" className="nav-link-custom">Home</Nav.Link>
+            <Nav.Link as={Link} to="/./pages/about" className="nav-link-custom">Our Story</Nav.Link>
+            <Nav.Link as={Link} to="/./pages/menu" className="nav-link-custom">Menu</Nav.Link>
+            <Nav.Link as={Link} to="/./pages/services" className="nav-link-custom">Services</Nav.Link>
+            <Nav.Link as={Link} to="/./pages/reservation" className="nav-link-custom">Reservation</Nav.Link>
+            <Nav.Link as={Link} to="/./pages/contact" className="nav-link-custom">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
